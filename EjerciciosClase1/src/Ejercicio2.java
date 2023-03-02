2ae65038a0b74cebdc1be6c795a33ba67d8b332d
Martin Alturria <martinalturria@hotmail.com>
2023-03-02 12:47:55 -0300

    Agrego Ejercicio 2 Finalizado

---
+++ EjerciciosClase1/src/Ejercicio2.java
@@ -0,0 +1,18 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("############");
+        System.out.println("EJERCICIO 2:");
+        System.out.println("############");
+
+        double ingreso = 480000;
+        int cantVehi = 1;
+        int cantInmue = 1;
+        boolean embarcAeoroActiv = true;
+
+        if(ingreso >= 489083 || cantVehi >= 3 || cantInmue >= 3 || embarcAeoroActiv){
+            System.out.println("Pertenece al Segmento de Ingresos Altos");
+        }else {
+            System.out.println("No Pertenece al Semento de Ingresos Altos");
+        }
+    }
+}
