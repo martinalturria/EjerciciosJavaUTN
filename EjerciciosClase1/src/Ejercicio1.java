f9b1a8158cfa7420b37c28af0e4dd5a26f53059c
Martin Alturria <martinalturria@hotmail.com>
2023-03-02 12:08:18 -0300

    Inicializo Ejercicios Clase 1

---
+++ EjerciciosClase1/src/Ejercicio1.java
@@ -0,0 +1,11 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 100;
+
+        while(a <= b) {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
