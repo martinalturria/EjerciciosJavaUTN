c7f133c461ae504177c98d69d492cdf3ac049f51
Martin Alturria <martinalturria@hotmail.com>
2023-03-02 12:27:55 -0300

    Agrego Solucion Ejercicio 1

--- EjerciciosClase1/src/Ejercicio1.java
+++ EjerciciosClase1/src/Ejercicio1.java
@@ -1,11 +1,57 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-        int a = 50;
+        System.out.println("##############");
+        System.out.println("EJERCICIO 1-A:");
+        System.out.println("##############");
+
+        int a = 80;
         int b = 100;
 
         while(a <= b) {
             System.out.println(a);
             a++;
+        }
+
+        System.out.println("##############");
+        System.out.println("EJERCICIO 1-B:");
+        System.out.println("##############");
+
+        a = 80;
+        b = 100;
+
+        while(a <= b) {
+            if (a % 2 == 0) {
+                System.out.println(a);
+            }
+            a++;
+        }
+
+        System.out.println("##############");
+        System.out.println("EJERCICIO 1-C:");
+        System.out.println("##############");
+
+        a = 80;
+        b = 100;
+        boolean pares = true;
+
+        while(a <= b){
+            if(pares && (a % 2 == 0)){
+                System.out.println(a);
+            }else if(!pares && a % 2 != 0){
+                System.out.println(a);
+            }
+            a++;
+        }
+
+        System.out.println("##############");
+        System.out.println("EJERCICIO 1-D:");
+        System.out.println("##############");
+
+        a = 80;
+        b = 100;
+
+        for(int i = b; i >= a; i--) {
+            System.out.println(i);
         }
     }
 }
